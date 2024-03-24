326d1d7679ff9f3eff1615912ed28ffc16e9f15a
Omarxyz <kimsanov.omadbek@gmail.com>
2024-03-24 15:33:02 +0500

    new route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-
+/* delete me */
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/my-site', function () {
     return view('welcome');
 });
